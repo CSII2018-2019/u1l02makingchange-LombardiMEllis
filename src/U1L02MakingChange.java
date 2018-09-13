129b9a3b6456442af39b4e112dd3afb5d38bb77f
Lombardi <Lombardi@172.16.20.251>
2018-09-13 09:31:35 -0400

    mathematical method - pennies not working?

--- src/U1L02MakingChange.java
+++ src/U1L02MakingChange.java
@@ -3,6 +3,8 @@ import javax.swing.JOptionPane;
 public class U1L02MakingChange {
 
 	public static void main(String[] args) {
+		/* USING A LOOP
+		 
 		// define variables for amounts of each coin
 		int nickels = 0;
 		int pennies = 0;
@@ -25,8 +27,28 @@ public class U1L02MakingChange {
 		JOptionPane.showMessageDialog(null, "$" + cash + " is "
 				+ nickels + " nickels and "
 				+ pennies + " pennies.");
+		*/
 		
-
+		
+		
+		//MATHEMATICAL METHOD
+		
+		//get input from user
+		String answer = JOptionPane.showInputDialog("Enter a monetary value (ex: 1.56): ");
+		double cash = Double.parseDouble(answer);
+		System.out.println(cash);
+		
+		//divide by 5 and round down
+		int nickels = (int)(cash/0.05);
+		//System.out.println(nickels);
+		
+		//subtract # of nickels times 5 from amount to get pennies
+		int pennies = (int) ((cash - (.05 * nickels)) * 100);
+		
+		//print answer
+		JOptionPane.showMessageDialog(null, "$" + cash + " is "
+				+ nickels + " nickels and "
+				+ pennies + " pennies.");
 	}
 
 }
