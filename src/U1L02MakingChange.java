95380e741be852be412eaa0b78b4408a65b5030c
Lombardi <Lombardi@172.16.20.251>
2018-09-13 08:53:07 -0400

    psuedocode loop

--- src/U1L02MakingChange.java
+++ src/U1L02MakingChange.java
@@ -2,7 +2,15 @@
 public class U1L02MakingChange {
 
 	public static void main(String[] args) {
-		// kjdsfhsf
+		// define variables for amounts of each coin
+		
+		//get input from user (change to usable value)
+		
+		//loop subtracting coin amount until money < coin amount
+		
+		//pennies = money * 100
+		
+		//print answer
 
 	}
 
